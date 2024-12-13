f9b0755d11bfa589efc28180a9b43989b1e7bc53
NhanVoo <thanhnhan@osogames.studio>
2024-12-14 00:05:12 +0700

    init project

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData")]
+public class LevelData : ScriptableObject
+{
+    public int             Id;
+    public List<PhaseData> PhaseDatas;
+}
+
+[Serializable]
+public class PhaseData
+{
+    public float  StartTime;
+    public string TileId;
+    public float  TimeBetween;
+}
---
+++ Assets/Scripts/TileDatabase.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace
+{
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "TileDatabase", menuName = "ScriptableObjects/TileDatabase")]
+    public class TileDatabase : ScriptableObject
+    {
+        public List<TileData> tileDatas = new();
+    }
+
+    [Serializable]
+    public class TileData
+    {
+        public string     Id;
+        public GameObject Prefab;
+    }
+}
