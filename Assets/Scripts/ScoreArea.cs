2c21d74d5bf9a0b1083ab01c8381db6adec56d17
NhanVoo <thanhnhan@osogames.studio>
2024-12-14 12:45:40 +0700

    adding score area

---
+++ Assets/Scripts/ScoreArea.cs
@@ -0,0 +1,21 @@
+﻿
+
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour, IScoreArea
+{
+    public enum ScoreType
+    {
+        Missed = 0,
+        Good = 1,
+        Great = 2,
+        Perfect = 3,
+    }
+
+    [SerializeField] private ScoreType scoreType;
+    
+    public void Score()
+    {
+        
+    }
+}
